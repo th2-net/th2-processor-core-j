b130e3e5cacb238c1cc849a93d1784b0ad31ff10
Oleg <oleg.smirnov@exactprosystems.com>
2024-03-18 17:16:26 +0400

    Migrate to th2 gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,24 @@
 @file:Suppress("SpellCheckingInspection")
 
-import com.github.jk1.license.filter.LicenseBundleNormalizer
-import com.github.jk1.license.render.JsonReportRenderer
-import org.gradle.api.JavaVersion.VERSION_11
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.net.URL
-
 plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("kapt") version "1.8.22"
     `java-library`
     `maven-publish`
-    signing
-    id("org.owasp.dependencycheck") version "8.3.1"
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-    id("com.gorylenko.gradle-git-properties") version "2.4.1"
-    id("com.github.jk1.dependency-license-report") version "2.5"
-    id("de.undercouch.download") version "5.4.0"
+    id("com.exactpro.th2.gradle.base") version "0.0.3"
+    id("com.exactpro.th2.gradle.publish") version "0.0.3"
 }
 
 group = "com.exactpro.th2"
-version = project.findProperty("release_version") ?: "1.0-SNAPSHOT"
+version = project.findProperty("release_version") as String
 
 java {
-    sourceCompatibility = VERSION_11
-    targetCompatibility = VERSION_11
-
     withJavadocJar()
     withSourcesJar()
+}
+
+kotlin {
+    jvmToolchain(11)
 }
 
 repositories {
@@ -49,8 +40,6 @@ repositories {
 }
 
 dependencies {
-    api(platform("com.exactpro.th2:bom:4.5.0"))
-
     api("com.exactpro.th2:grpc-common:4.3.0-dev")
     implementation("com.exactpro.th2:common:5.7.2-dev")
     implementation("com.exactpro.th2:common-utils:2.2.2-dev")
@@ -73,82 +62,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            pom {
-                name.set(rootProject.name)
-//                packaging.set("jar")
-                description.set(rootProject.description)
-                url.set(project.findProperty("vcs_url").toString())
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("developer")
-                        name.set("developer")
-                        email.set("developer@exactpro.com")
-                    }
-                }
-                scm {
-                    url.set(project.findProperty("vcs_url").toString())
-                }
-            }
-        }
-    }
-    repositories {
-//Nexus repo to publish from gitlab
-        maven {
-            name = "nexusRepository"
-            credentials {
-                username = project.findProperty("nexus_user").toString()
-                password = project.findProperty("nexus_password").toString()
-            }
-            url = uri(project.findProperty("nexus_url").toString())
-        }
-    }
-}
-
-signing {
-    useInMemoryPgpKeys(
-        project.findProperty("signingKey").toString(),
-        project.findProperty("signingPassword").toString()
-    )
-    sign(publishing.publications["maven"])
-}
-
-nexusPublishing {
-    repositories {
-        create("sonatype") {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-        }
-    }
-}
-
 tasks {
     test {
         useJUnitPlatform {
             excludeTags("integration-test")
-        }
-    }
-
-    jar {
-        manifest {
-            attributes(
-                "Created-By" to "${System.getProperty("java.version")} (${System.getProperty("java.vendor")})",
-                "Specification-Title" to "",
-                "Specification-Vendor" to "Exactpro Systems LLC",
-                "Implementation-Title" to project.displayName,
-                "Implementation-Vendor" to "Exactpro Systems LLC",
-                "Implementation-Vendor-Id" to "com.exactpro",
-                "Implementation-Version" to project.version
-            )
         }
     }
 
@@ -158,62 +75,4 @@ tasks {
             includeTags("integration-test")
         }
     }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
-    }
-
-    // conditionals for publications
-    withType<PublishToMavenRepository> {
-        onlyIf {
-            (repository == publishing.repositories["nexusRepository"] &&
-                    project.hasProperty("nexus_user") &&
-                    project.hasProperty("nexus_password") &&
-                    project.hasProperty("nexus_url")) ||
-                    (repository == publishing.repositories["sonatype"] &&
-                            project.hasProperty("sonatypeUsername") &&
-                            project.hasProperty("sonatypePassword"))
-        }
-    }
-    withType<Sign> {
-        onlyIf { project.hasProperty("signingKey") &&
-                project.hasProperty("signingPassword")
-        }
-    }
-}
-
-dependencyLocking {
-    lockAllConfigurations()
-}
-
-dependencyCheck {
-    formats = listOf("SARIF", "JSON", "HTML")
-    failBuildOnCVSS = 5F
-
-    analyzers.apply {
-        assemblyEnabled = false
-        nugetconfEnabled = false
-        nodeEnabled = false
-    }
-}
-
-licenseReport {
-    val licenseNormalizerBundlePath = "$buildDir/license-normalizer-bundle.json"
-
-    if (!file(licenseNormalizerBundlePath).exists()) {
-        download.run {
-            src("https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/license-normalizer-bundle.json")
-            dest("$buildDir/license-normalizer-bundle.json")
-            overwrite(false)
-        }
-    }
-
-    filters = arrayOf(
-        LicenseBundleNormalizer(licenseNormalizerBundlePath, false)
-    )
-    renderers = arrayOf(
-        JsonReportRenderer("licenses.json", false),
-    )
-    excludeOwnGroup = false
-    allowedLicensesFile = URL("https://raw.githubusercontent.com/th2-net/.github/main/license-compliance/gradle-license-report/allowed-licenses.json")
 }
